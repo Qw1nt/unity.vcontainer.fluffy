a2448a3bd5a89043561870efe7bd670eb8639f41
Qw1nt <pasharudnykh3@mail.ru>
2023-09-01 14:37:36 +0500

    [+] Инициализация

---
+++ Runtime/DiContainerBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace Runtime.DI.Core
+{
+    public abstract class DiContainerBase : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            FindAutoInjectObject();
+            AddDependencies(builder);
+        }
+
+        protected abstract void AddDependencies(IContainerBuilder builder);
+
+        private void FindAutoInjectObject()
+        {
+            var objects = FindObjectsByType<AutoInject>(FindObjectsInactive.Include, FindObjectsSortMode.None)
+                .Select(x => x.gameObject).ToList();
+            autoInjectGameObjects = objects;
+        }
+    }
+}
